8bf2cd7bd178591984012bb1a01505b6b50e0c79
sreekar <sreekardugyala@my.unt.edu>
2025-01-13 16:22:11 -0600

    Jan13-Intro to WebFlux and Reactor Core

---
+++ src/main/java/com/sreekar/WebFluxAndReactorCore/WebFluxAndReactorCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sreekar.WebFluxAndReactorCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxAndReactorCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxAndReactorCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreekar/WebFluxAndReactorCore/WebFluxAndReactorCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreekar.WebFluxAndReactorCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxAndReactorCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
